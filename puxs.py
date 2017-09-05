8b626aee26c87963bdb184537667e0efee9973ce
Universal ROM Tools <david.legras@gmail.com>
2017-09-05 23:25:07 +0200

    First real comit

--- puxs.py
+++ puxs.py
@@ -1,20 +1,22 @@
-from puxsroms import catchssxml
-from xml.etree import ElementTree
+"""Main Script"""
+from romfile import romfile
+from ssdownload import downloadfile
+from ssdownload import urlconstruct
+from os.path import splitext
+import logging
 
-import encryptor
-
-print(login)
-
-# catchssxml('aof.zip', 75)
-# with open('test.xml', 'rt') as f:
-#     tree = ElementTree.parse(f)
-#
-# for node in tree.iter('media'):
-#     xmlparent = node.attrib.get('parent')
-#     xmltype = node.attrib.get('type')
-#     xmlregion = node.attrib.get('region')
-#     if xmlparent=='jeu' and xmltype=='flyer' and xmlregion=='wor':
-#         print(' %s' % xmlparent)
-#         print('     %s' % xmltype)
-#         print('        %s' % xmlregion)
-#         print('------------> %s' % node.text)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='log.txt',level=logging.DEBUG)
+rom = romfile("Sonic The Hedgehog (USA, Europe).zip")
+logging.debug("parentpath : {}".format(rom.parentpath))
+logging.info("name :{}".format(rom.name))
+logging.debug("size :{}".format(rom.size))
+logging.debug("crc32 :{}".format(rom.crc32))
+logging.debug("md5 :{}".format(rom.md5))
+logging.debug("sha1 :{}".format(rom.sha1))
+ssurl = urlconstruct(rom,1)
+ssxml = splitext(rom.name)[0] + '.xml'
+result = downloadfile(ssurl, ssxml)
+if result == True:
+    logging.info("download OK")
+else:
+    logging.warning(result)
---
+++ ssdownload.py
@@ -0,0 +1,72 @@
+"""
+Function to download from ScreenScraper
+    return True or Error message
+"""
+
+from urllib import request
+from urllib import parse
+from urllib import error
+from romfile import romfile
+from os.path import splitext
+from base64 import b64decode
+import pickle
+
+
+def downloadfile(ssurl, ssxml):
+    screenscrapererror = {400: "URL missing parameters",
+                          401: "API closed for non suscriber user",
+                          403: "Dev login error",
+                          404: "Game not found",
+                          423: "API closed",
+                          426: "Scrapper version obsolete",
+                          429: "Maximum threads allowed already used"}
+    try:
+        con = request.urlopen(ssurl)
+    except error.HTTPError as e:
+        return ("{} - HTTP error : {}".format(e.code, screenscrapererror[e.code]))
+    except error.URLError as e:
+        return ("URL error : {}".format(e.reason))
+    else:
+        request.urlretrieve(ssurl, ssxml)
+        return True
+
+
+def urlconstruct(rom, systemid='', ssid='', sspassword=''):
+    with open('devlogin', 'rb') as fichier:
+        devlogin = pickle.Unpickler(fichier).load()
+
+    ssurl = "https://www.screenscraper.fr/api2/jeuInfos.php?" \
+            "devid={}" \
+            "&devpassword={}" \
+            "&softname=PUXS" \
+            "&output=xml" \
+            "&ssid={}" \
+            "&sspassword={}" \
+            "&crc={}" \
+            "&md5={}" \
+            "&sha1={}" \
+            "&systemeid={}" \
+            "&romtype=rom" \
+            "&romnom={}" \
+            "&romtaille={}" \
+        .format(b64decode(devlogin["devid"]).decode("utf-8", "ignore"),
+                b64decode(devlogin["devpassword"]).decode("utf-8", "ignore"),
+                ssid,
+                sspassword,
+                rom.crc32,
+                rom.md5,
+                rom.sha1,
+                systemid,
+                parse.quote_plus(rom.name), rom.size)
+    return ssurl
+
+
+if __name__ == "__main__":
+    rom = romfile("Sonic The Hedgehog (USA, Europe).zip")
+    ssurl = urlconstruct(rom,1)
+    ssxml = splitext(rom.name)[0] + '.xml'
+    result = downloadfile(ssurl, ssxml)
+    if result == True:
+        print("download OK")
+    else:
+        print(result)
