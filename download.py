29b7bbdfe09c292f3c885f96747a3d7f9ca571ca
Universal ROM Tools <david.legras@gmail.com>
2017-09-08 10:11:09 +0200

    listing file OK

--- download.py
+++ download.py
@@ -10,38 +10,119 @@ from os.path import splitext
 from base64 import b64decode
 import pickle
 
+httperrordef = {100: "Continue",
+                101: "Switching Protocols",
+                102: "Processing",
+                200: "OK",
+                201: "Created",
+                202: "Accepted",
+                203: "Non-Authoritative Information",
+                204: "No Content",
+                205: "Reset Content",
+                206: "Partial Content",
+                207: "Multi-Status",
+                208: "Already Reported",
+                210: "Content Different",
+                226: "IM Used",
+                300: "Multiple Choices",
+                301: "Moved Permanently",
+                302: "Moved Temporarily",
+                303: "See Other",
+                304: "Not Modified",
+                305: "Use Proxy",
+                306: "(aucun)",
+                307: "Temporary Redirect",
+                308: "Permanent Redirect",
+                310: "Too many Redirects",
+                400: "Bad Request",
+                401: "Unauthorized",
+                402: "Payment Required",
+                403: "Forbidden",
+                404: "Not Found",
+                405: "Method Not Allowed",
+                406: "Not Acceptable",
+                407: "Proxy Authentication Required",
+                408: "Request Time-out",
+                409: "Conflict",
+                410: "Gone",
+                411: "Length Required",
+                412: "Precondition Failed",
+                413: "Request Entity Too Large",
+                414: "Request-URI Too Long",
+                415: "Unsupported Media Type",
+                416: "Requested range unsatisfiable",
+                417: "Expectation failed",
+                418: "I’m a teapot",
+                421: "Bad mapping / Misdirected Request",
+                422: "Unprocessable entity",
+                423: "Locked",
+                424: "Method failure",
+                425: "Unordered Collection",
+                426: "Upgrade Required",
+                428: "Precondition Required",
+                429: "Too Many Requests",
+                431: "Request Header Fields Too Large",
+                449: "Retry With",
+                450: "Blocked by Windows Parental Controls",
+                451: "Unavailable For Legal Reasons",
+                456: "Unrecoverable Error",
+                444: "No Response",
+                495: "SSL Certificate Error",
+                496: "SSL Certificate Required",
+                497: "HTTP Request Sent to HTTPS Port",
+                499: "Client Closed Request",
+                500: "Internal Server Error",
+                501: "Not Implemented",
+                502: "Bad Gateway ou Proxy Error",
+                503: "Service Unavailable",
+                504: "Gateway Time-out",
+                505: "HTTP Version not supported",
+                506: "Variant Also Negotiates",
+                507: "Insufficient storage",
+                508: "Loop detected",
+                509: "Bandwidth Limit Exceeded",
+                510: "Not extended",
+                511: "Network authentication required"}
 
-def ssdownloadfile(ssurl, ssxml):
+sserrordef = {400: "URL missing parameters",
+              401: "API closed for non suscriber user",
+              403: "Dev login error",
+              404: "Game not found",
+              423: "API closed",
+              426: "Scrapper version obsolete",
+              429: "Maximum threads allowed already used"}
+
+
+def downloadfile(url, path):
     """
-    this function will download the ssurl to the ssxml
+    this function will download the url to the xml
     return True or Error message
     """
-    screenscrapererror = {400: "URL missing parameters",
-                          401: "API closed for non suscriber user",
-                          403: "Dev login error",
-                          404: "Game not found",
-                          423: "API closed",
-                          426: "Scrapper version obsolete",
-                          429: "Maximum threads allowed already used"}
+    
+    if "screenscraper.fr" in url:
+        errordef = sserrordef
+    else:
+        errordef = httperrordef
+
     try:
-        con = request.urlopen(ssurl)
+        con = request.urlopen(url)
     except error.HTTPError as e:
-        return ("{} - HTTP error : {}".format(e.code, screenscrapererror[e.code]))
+        return ("{} - HTTP error : {}".format(e.code, errordef[e.code]))
     except error.URLError as e:
         return ("URL error : {}".format(e.reason))
     else:
-        request.urlretrieve(ssurl, ssxml)
+        request.urlretrieve(url, path)
         return True
 
 
-def ssurlconstruct(rom, systemid='', ssid='', sspassword=''):
+def urlconstruct(rom, systemid='', ssid='', sspassword=''):
     """
-    this function will return the constructed ssurl
+    this function will return the constructed screecnscraper url
     """
     with open('devlogin', 'rb') as fichier:
         devlogin = pickle.Unpickler(fichier).load()
 
-    ssurl = "https://www.screenscraper.fr/api2/jeuInfos.php?" \
+    url = "https://www.screenscraper.fr/api2/jeuInfos.php?" \
             "devid={}" \
             "&devpassword={}" \
             "&softname=PUXS" \
@@ -64,14 +145,14 @@ def ssurlconstruct(rom, systemid='', ssid='', sspassword=''):
                 rom.sha1,
                 systemid,
                 parse.quote_plus(rom.name), rom.size)
-    return ssurl
+    return url
 
 
 if __name__ == "__main__":
     rom = romfile("Sonic The Hedgehog (USA, Europe).zip")
-    ssurl = ssurlconstruct(rom,1)
-    ssxml = splitext(rom.name)[0] + '.xml'
-    result = ssdownloadfile(ssurl, ssxml)
+    url = urlconstruct(rom, 1)
+    path = splitext(rom.name)[0] + '.xml'
+    result = downloadfile(url, path)
     if result == True:
         print("download OK")
     else:
--- puxs.py
+++ puxs.py
@@ -1,24 +1,46 @@
 """Main Script"""
+
 from romfile import romfile
-from download import ssdownloadfile
-from download import ssurlconstruct
+from download import downloadfile
+from download import urlconstruct
 from xmlreader import xmlvalue
 from os.path import splitext
+from os.path import join
+from os import walk
 import logging
+import sys
+import pathlib
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='log.txt', filemode='w', level=logging.DEBUG)
-rom = romfile("Sonic The Hedgehog (USA, Europe).zip")
-logging.debug("parentpath : {}".format(rom.parentpath))
-logging.info("name :{}".format(rom.name))
-logging.debug("size :{}".format(rom.size))
-logging.debug("crc32 :{}".format(rom.crc32))
-logging.debug("md5 :{}".format(rom.md5))
-logging.debug("sha1 :{}".format(rom.sha1))
-ssurl = ssurlconstruct(rom, 1)
-ssxml = splitext(rom.name)[0] + '.xml'
-result = ssdownloadfile(ssurl, ssxml)
-if result == True:
-    logging.info("download OK")
-    xmlvalue(ssxml, 'media', '', 'jeu', 'box-2D', 'eu')
-else:
-    logging.warning(result)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                    filename='log.txt', filemode='w', level=logging.DEBUG)
+
+# this is extension you want to detect
+extension = (".zip", ".gen", ".bin", ".MD")
+# This is the path where you want to search
+path = r'C:\Developpement\Github\roms\megadrive'
+
+pathlib.Path(join(sys.path[0],'cache','xml')).mkdir(parents=True, exist_ok=True)
+filenames = []
+
+for root, dirs, files in walk(path):
+    for file in files:
+        if file.endswith(extension):
+            filenames.append(join(root, file)[len(path) + 1:])
+
+for files in filenames:
+    logging.info("files : {}".format(join(path,files)))
+    rom = romfile(join(path,files))
+    logging.debug("parentpath : {}".format(rom.parentpath))
+    logging.info("name :{}".format(rom.name))
+    logging.debug("size :{}".format(rom.size))
+    logging.debug("crc32 :{}".format(rom.crc32))
+    logging.debug("md5 :{}".format(rom.md5))
+    logging.debug("sha1 :{}".format(rom.sha1))
+    url = urlconstruct(rom, 1)
+    xmlpath = join(sys.path[0],'cache','xml',splitext(rom.name)[0] + '.xml')
+    result = downloadfile(url, xmlpath)
+    if result == True:
+        logging.info("download OK")
+        xmlvalue(xmlpath, 'media', '', 'jeu', 'box-2D', 'eu')
+    else:
+        logging.warning(result)
